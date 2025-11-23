5163aa9facc4687ac247df10a127d139db66642f
= <yuvraj1165@gmail.com>
2025-11-23 11:34:50 +0000

    Refactored the TC

--- Supermarket.Tests/CheckoutServiceTests.cs
+++ Supermarket.Tests/CheckoutServiceTests.cs
@@ -3,25 +3,26 @@ namespace Supermarket.Tests;
 [TestFixture]
 public class Tests
 {
+    CheckoutService _checkoutService;
+
     [SetUp]
     public void Setup()
     {
+        _checkoutService = new CheckoutService();
     }
 
     [Test]
     public void GivenEmptyBasket_WhenCheckoutDone_TotalShouldIsZero()
     {
-        var basketTotal = new CheckoutService().GetTotalPrice();
-
+        var basketTotal = _checkoutService.GetTotalPrice();
         Assert.That(basketTotal, Is.EqualTo(0));
     }
 
     [Test]
     public void GivenOneItemA_WhenCheckoutDone_TotalShouldBe50()
     {
-        var checkout = new CheckoutService();
-        checkout.Scan("A");
-        var basketTotal = checkout.GetTotalPrice();
+        _checkoutService.Scan("A");
+        var basketTotal = _checkoutService.GetTotalPrice();
         Assert.That(basketTotal, Is.EqualTo(50));
     }
 }
