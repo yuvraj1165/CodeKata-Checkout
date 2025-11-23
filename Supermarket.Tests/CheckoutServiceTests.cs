30f0637665e3c9cbdaf0d36276f0cb81c934e68f
= <yuvraj1165@gmail.com>
2025-11-23 11:23:30 +0000

    Red-Added TC for validating Item A unit price

--- Supermarket.Tests/CheckoutServiceTests.cs
+++ Supermarket.Tests/CheckoutServiceTests.cs
@@ -15,4 +15,13 @@ public class Tests
 
         Assert.That(basketTotal, Is.EqualTo(0));
     }
+
+    [Test]
+    public void GivenOneItemA_WhenCheckoutDone_TotalShouldBe50()
+    {
+        var checkout = new CheckoutService();
+        checkout.Scan("A");
+        var basketTotal = checkout.GetTotalPrice();
+        Assert.That(basketTotal, Is.EqualTo(50));
+    }
 }
